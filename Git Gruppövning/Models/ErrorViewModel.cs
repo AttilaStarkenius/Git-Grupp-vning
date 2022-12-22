483aaea3c61938c4a72918690e568c03461eb569
Attila Starkenius <symphtom@hotmail.com>
2022-12-22 06:53:16 +0100

    Add project files.

---
+++ Git Gruppövning/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Git_Gruppövning.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
